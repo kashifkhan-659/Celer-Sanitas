3da9cbac1aa25db11b4749af5246abf9da79bed0
MZainUlAbidin-25i3086 <moh.zainulabidin550@gmail.com>
2026-08-31 16:14:37 +0500

    redesign doctor dashboard to match Figma frame, add logo

--- client/src/components/shared/Layout.jsx
+++ client/src/components/shared/Layout.jsx
@@ -4,13 +4,34 @@
 
 import Disclaimer from './Disclaimer.jsx';
 
-// Inline SVG so we don't ship an image file for one small mark.
-const Mark = () => (
-  <svg width="30" height="30" viewBox="0 0 64 64" aria-hidden="true">
+// Inline SVG rather than a file in /public: one less network request, no flash of missing
+// logo on first paint. The gradient id is namespaced so it can't collide with other SVGs.
+const Mark = ({ size = 30 }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="100 40 460 440"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
     <path
-      d="M8 52 Q10 20 44 6 Q34 26 30 36 Q46 26 58 14 Q46 40 34 50 Q22 58 8 52 Z"
-      fill="#1F7A70"
+      d="M123 359C202.117 219.365 296.604 138.806 492.5 52C429.294 119.39 388.032 167.399 355.921 217.064C348.017 229.555 340.571 242.046 333.643 254.421C323.343 273.21 313.832 292.901 304.5 314.5C409.069 255.091 460.948 215.042 543.5 133.5C472.062 293.478 355.5 432 290 464C251 474 162.157 413.179 123 359Z"
+      fill="url(#celerMarkGradient)"
     />
+    <defs>
+      <linearGradient
+        id="celerMarkGradient"
+        x1="333.25"
+        y1="52"
+        x2="333.25"
+        y2="465.088"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#164F49" />
+        <stop offset="1" stopColor="#2A9D8F" />
+      </linearGradient>
+    </defs>
   </svg>
 );
 
